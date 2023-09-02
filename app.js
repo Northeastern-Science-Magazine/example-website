40c3e052a3d4c5d59d6d29a667a8d872c3ba6884
Ethan Szeto <ethan.szeto@gmail.com>
2023-09-02 15:15:58 -0400

    New imports

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import route from "./routes/pages.route.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(cookieParser())
+app.use(bodyParser.json());
 
 app.use("/", route);
 
